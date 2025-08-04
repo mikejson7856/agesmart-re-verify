dc8f99fba75fb326b4651a00f61d5b2380ced0df
rana <rana.buddy@gmail.com>
2025-08-04 16:33:37 +0600

    feat:xioncart sh creation

--- app/components/First.js
+++ app/components/First.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation";
+
 export default function First() {
 
       const router = useRouter();
